194b111a0c83f79062acce04a0d6852789b39868
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-04-10 14:41:27 +0200

    Add int64_t parsing benchmark

--- Makefile
+++ Makefile
@@ -3,19 +3,20 @@ LDFLAGS=-flto
 
 RM ?= rm -rf
 
-all: check-float bench-float debug-float
+all: check-float bench-float bench-int debug-float
 
 check: check-float
 	./check-float
 
-bench: bench-float
-	./bench-float
+bench: bench-float bench-int
+	./bench-float && ./bench-int
 
 debug: debug-float
 
 check-float: check-float.c parse-float.h
 debug-float: debug-float.c parse-float.h
 bench-float: bench-float.c parse-float.h
+bench-int: bench-int.c parse-int.h
 
 clean:
 	$(RM) check-float
---
+++ bench-int.c
@@ -0,0 +1,135 @@
+/* Copyright (C) 2018 Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
+ * Released under the Mozilla Public License version 2.0
+ */
+
+/* Script to benchmark parsing of a large variety of floating-point
+ * values, comparing the performance of fast_parse_float32
+ * with the performance of the standard library.
+ */
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "parse-int.h"
+
+size_t fill_buffer(char *buffer, size_t size, int64_t start, int64_t end)
+{
+	size_t pos = 0;
+	memset(buffer, 0, size);
+
+	for (int64_t i = start; i < end; ++i)
+		pos += snprintf(buffer + pos, size - pos, "%"PRId64"/%"PRId64"/%"PRId64"\n", i-1, i, i+1);
+	return pos;
+}
+
+/* Parse the buffer using fast_parse_int64, returns the number of parsed floats
+ */
+size_t fast_parse_buffer(const char *buffer, int64_t *results, size_t max_ints)
+{
+	const char *from = NULL;
+	char *end = (char*)buffer;
+	size_t i;
+	for (i = 0; i < max_ints && end != from; ++i) {
+		from = end;
+		if (*from == '/') ++from;
+		else while (isspace(*from)) ++from;
+		results[i] = fast_parse_int64(from, &end);
+	}
+	return i;
+}
+
+/* Parse the buffer using strtof, returns the number of parsed floats
+ */
+size_t std_parse_buffer(const char *buffer, int64_t *results, size_t max_ints)
+{
+	const char *from = NULL;
+	char *end = (char*)buffer;
+	size_t i;
+	for (i = 0; i < max_ints && end != from; ++i) {
+		from = end;
+		if (*from == '/') ++from;
+		results[i] = strtol(from, &end, 10);
+	}
+	return i;
+}
+
+void report_performance(clock_t tics, const char *op, size_t size, const char *expr)
+{
+	double runtime = tics;
+	runtime /= CLOCKS_PER_SEC;
+	printf("%s: %zu in %gsec, %gM%s\n", op, size, runtime, size/runtime/1.0e6, expr);
+	fflush(stdout);
+}
+
+void compare_results(const int64_t * restrict fast, const int64_t * restrict std, size_t num_fast, size_t num_std)
+{
+	size_t count = num_fast;
+	if (num_fast > num_std)
+		count = num_std;
+	if (num_fast != num_std)
+		fprintf(stderr, "fast/std count mismatch, using %zu of %zu/%zu\n", count, num_fast, num_std);
+	for (size_t i = 0; i < count; ++i) {
+		if (fast[i] != std[i])
+			fprintf(stderr, "misparsed %zu: %"PRId64" vs %"PRId64"\n", i, fast[i], std[i]);
+	}
+}
+
+int main()
+{
+#define BUFSIZE UINT32_C(1024*1024*1024)
+#define NUMINTS INT64_C(1024*1024*16)
+#define NUMRESULTS (3*NUMINTS)
+
+	char *buffer = malloc(BUFSIZE);
+	int64_t *fastresults = calloc(NUMRESULTS, sizeof(int64_t));
+	int64_t *stdresults = calloc(NUMRESULTS, sizeof(int64_t));
+
+	if (!buffer) {
+		fprintf(stderr, "failed to allocate buffer\n");
+		exit(-1);
+	}
+	if (!fastresults || !stdresults) {
+		fprintf(stderr, "failed to allocate results\n");
+		exit(-1);
+	}
+
+	size_t fast_parsed, std_parsed, filled_bytes;
+
+	clock_t tic, toc;
+
+	const int64_t starts[] = {
+		-NUMINTS*NUMINTS, -NUMINTS*NUMINTS/1024, -NUMINTS*NUMINTS/(1024*1024), -NUMINTS,
+		0,
+		NUMINTS, NUMINTS*NUMINTS/(1024*1024), NUMINTS*NUMINTS/1024, NUMINTS*NUMINTS,
+		INT64_MAX };
+
+	for (const int64_t *start = starts; *start != INT64_MAX; ++start) {
+		printf("====> Starting from %"PRId64"\n", *start);
+		fflush(stdout);
+
+		tic = clock();
+		filled_bytes = fill_buffer(buffer, BUFSIZE, *start, *start + NUMINTS);
+		toc = clock();
+		report_performance(toc - tic, "Clear and fill buffer", BUFSIZE + filled_bytes, "B/sec");
+
+		tic = clock();
+		fast_parsed = fast_parse_buffer(buffer, fastresults, NUMRESULTS);
+		toc = clock();
+		report_performance(toc - tic, "Fast parse", fast_parsed, "int64/sec");
+
+		tic = clock();
+		std_parsed = std_parse_buffer(buffer, stdresults, NUMRESULTS);
+		toc = clock();
+		report_performance(toc - tic, "Std parse", std_parsed, "int64/sec");
+
+		tic = clock();
+		compare_results(fastresults, stdresults, fast_parsed, std_parsed);
+		toc = clock();
+		report_performance(toc - tic, "Comparison", std_parsed, "int64/sec");
+	}
+
+	return 0;
+}
